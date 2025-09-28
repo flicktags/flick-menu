91f7fb8df9f663fcf65024610ade26fa9b12bc17
System Administrator <root@MUHAMMADs-MacBook-Pro.local>
2025-09-28 19:02:12 +0300

    Fix Firebase integration and update registerUser API

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,5 @@
 import admin from "firebase-admin";
-import serviceAccount from "./firebaseServiceAccount.json" with { type: "json" };
+// import serviceAccount from "./firebaseServiceAccount.json" with { type: "json" };
 
 admin.initializeApp({
   credential: admin.credential.cert(process.env.FIREBASE_CONFIG ? JSON.parse(process.env.FIREBASE_CONFIG) : serviceAccount),
