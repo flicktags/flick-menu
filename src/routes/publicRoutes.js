b8f2b8e973daba8dabee1a1a9b2c24dca3b14134
flicktags <flick.tags@gmail.com>
2025-10-18 12:54:43 +0300

    Update publicRoutes.js code to fix the issue

--- src/routes/publicRoutes.js
+++ src/routes/publicRoutes.js
@@ -1,15 +1,33 @@
+// // src/routes/publicRoutes.js
+// import express from "express";
+// import {
+//   getPublicMenu,
+//   getPublicSectionItems,
+// } from "../controllers/publicMenuController.js";
+
+// const router = express.Router();
+
+// router.get("/menu", getPublicMenu);
+// router.get("/menu/items", getPublicSectionItems);
+// router.post("/orders", createOrder);
+
+
+// export default router;
 // src/routes/publicRoutes.js
 import express from "express";
 import {
   getPublicMenu,
   getPublicSectionItems,
 } from "../controllers/publicMenuController.js";
+import { createOrder } from "../controllers/orderController.js";
 
 const router = express.Router();
 
+// Public menu endpoints
 router.get("/menu", getPublicMenu);
 router.get("/menu/items", getPublicSectionItems);
-PublicRouter.post("/orders", createOrder);
 
+// Public order creation
+router.post("/orders", createOrder);
 
 export default router;
