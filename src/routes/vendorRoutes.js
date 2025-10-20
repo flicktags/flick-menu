50bcabb286787acc01b8f3c7024bad09baff206b
flicktags <flick.tags@gmail.com>
2025-10-20 21:43:22 +0300

    updated codes for branch login

--- src/routes/vendorRoutes.js
+++ src/routes/vendorRoutes.js
@@ -1,7 +1,17 @@
+// import express from "express";
+// import { registerVendor } from "../controllers/authController.js";
+
+// const vendorRouter = express.Router();
+// vendorRouter.post("/register", registerVendor);
+
+// export default vendorRouter;
+// routes/vendorRoute.js
 import express from "express";
 import { registerVendor } from "../controllers/authController.js";
 
 const vendorRouter = express.Router();
+
+// POST /api/vendor/register
 vendorRouter.post("/register", registerVendor);
 
 export default vendorRouter;
