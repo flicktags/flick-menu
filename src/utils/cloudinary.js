f282160178105b0b355dbd1cccef14da4e439db9
flicktags <flick.tags@gmail.com>
2026-01-04 21:34:47 +0300

    bug fixed

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -2,9 +2,9 @@
 import { v2 as cloudinary } from "cloudinary";
 
 // ✅ Put your values here (manual)
-const CLOUDINARY_CLOUD_NAME = "myCLoudName";
-const CLOUDINARY_API_KEY = "myApiKey";
-const CLOUDINARY_API_SECRET = "myApiSecret";
+const CLOUDINARY_CLOUD_NAME = "diwspe6yi";
+const CLOUDINARY_API_KEY = "457834299199625";
+const CLOUDINARY_API_SECRET = "cOt4I5PxEZ7fPWOI0vmzXYzLt6o";
 
 // ✅ Your upload folder (same as Flutter folder you use)
 export const CLOUDINARY_FOLDER = "FlickMenu-Item-Image"; 
